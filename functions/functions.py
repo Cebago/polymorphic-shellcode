42e3b7ce66d4a70e8f721cdb87dcb3b0c3e09802
DEMARCHE Baptiste <bdemarche@myges.fr>
2022-02-11 15:02:17 +0100

    update structure

--- functions/functions.py
+++ functions/functions.py
@@ -1,1 +1,1 @@
-let registers = [rax, rbx, rcx, rdx, rdi, rsi, r8, r9, r10, r11, r12, r13, r14, r15]
+registers = ["rax", "rbx", "rcx", "rdx", "rdi", "rsi", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"]
---
+++ polymorph.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from functions.functions import *
+
+print("Welcome to the Polymorphic Shellcode")
