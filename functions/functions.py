0fffab370638e75c7176491b1fa0625dcbbcd625
DEMARCHE Baptiste <bdemarche@myges.fr>
2022-02-11 16:37:59 +0100

    function to select file

--- functions/functions.py
+++ functions/functions.py
@@ -1,1 +1,22 @@
+import os
+import string
+
 registers = ["rax", "rbx", "rcx", "rdx", "rdi", "rsi", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"]
+
+def selectFile(folder):
+    while True:
+        files = os.listdir('./'+folder)
+        for x in range(len(files)):
+            print(str(x+1)+"- "+files[x])
+        print("!q- Quit")
+        file = input("choice: ")
+        try:
+            x = int(file)
+            if (x >= 1 and x <= len(files)):
+                return file
+            else:
+                print("Not a good value")                
+        except ValueError:
+            if file == "!q":
+                return False
+            print("Not a good value")
