d3f670ba1f7091fb40eb51f250ba427e6830bb2b
DEMARCHE Baptiste <bdemarche@myges.fr>
2022-02-11 16:40:01 +0100

    menu option 1 ok

--- polymorph.py
+++ polymorph.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
-from functions.functions import *
+from functions.functions import *;
 
 print("Welcome to the Polymorphic Shellcode Tool from the Cebago TEAM")
-print("What do you want to do ?")
-print("1- Polymorph a file")
-print("2- See a polymorphed file")
-print("3- Generate the shellcode")
-print("4- Quit")
+
+quit = False
+choice = 0
+
+while not quit:
+    print("What do you want to do ?")
+    print("1- Polymorph a file")
+    print("2- See a polymorphed file")
+    print("3- Generate the shellcode")
+    print("4- Quit")
+    
+    choice = input("choice: ")
+    
+    if choice == "1":
+        print("\n\n\n=====Polymorph File=====")
+        print("Which file do you want to polymorph ?")
+        file = selectFile("input")
+        if file != False:
+            #goToFunction
+            print("goToFunction")
+        print("=====End Polymorph File=====\n\n\n")
+    elif choice == "2":
+        print("See")
+    elif choice == "3":
+        print("Generated")
+    elif choice == "4":
+        quit = True
+    else:
+        print("Bad choice")
