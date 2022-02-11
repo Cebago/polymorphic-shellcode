db09610c870cdcbd07cba6360437a839aa3278a2
DEMARCHE Baptiste <bdemarche@myges.fr>
2022-02-11 18:18:03 +0100

    change menu

--- polymorph.py
+++ polymorph.py
@@ -11,7 +11,41 @@ while not quit:
     print("1- Polymorph a file")
     print("2- See a polymorphed file")
     print("3- Generate the shellcode")
-    print("4- Quit")
+    print("4- Verify a shellcode")
+    print("5- Quit")
     
+    choice = input("choice: ")
+    
+    if choice == "1":
+        print("\n\n\n=====Polymorph File=====")
+        print("Which file do you want to polymorph ?")
+        file = selectFile("input")
+        if file != False:
+            #goToFunctionPolymorph
+            print("goToFunction")
+        print("=====End Polymorph File=====\n\n\n")
+    elif choice == "2":
+        print("\n\n\n=====See Assembly File=====")
+        print("Which file do you want to see ?")
+        file = selectFile("output")
+        if file != False:
+            displayFile("output", file)
+            print("goToFunction")
+        print("=====End See Assembly File=====\n\n\n")
+    elif choice == "3":
+        print("Generated")
+    elif choice == "4":
+        print("\n\n\n=====Shellcode scanner=====")
+        print("Which file do you want to scan ?")
+        file = selectFile("shellcode")
+        if file != False:
+            verifyFile("shellcode", file)
+            print("goToFunction")
+        print("=====End Shellcode scanner=====\n\n\n")
+    elif choice == "5":
+        quit = True
+    else:
+        print("Bad choice")
+
 search = openDico()
 readAsm(search)
