86243c32f66e9f977f49a205048f933347227dd9
Baptiste DEMARCHE <baptistedemarche@bdemarche.fr>
2022-03-05 13:36:08 +0100

    delete global var

--- functions/functions.py
+++ functions/functions.py
@@ -4,7 +4,6 @@ import json
 import random
 
 registers = ["rax", "rbx", "rcx", "rdx", "rdi", "rsi", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"]
-reg = []
 
 def selectFile(folder):
     while True:
